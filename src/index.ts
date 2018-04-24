c3c0bd63466a5e0486cf5f8fbea0fab51b5fac67
manuel binz <manuel.binz@gmail.com>
2018-04-24 13:00:02 +0200

    feat(package): index.ts

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export * from './caching-observable.module';
+export * from './caching-observable.service';
+export * from './storage-driver/local-storage.service';
+export * from './storage-driver/session-storage.service';
