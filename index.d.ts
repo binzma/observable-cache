c596813ba2cdef7dfb425ccf0d052d33af647e86
manuel binz <manuel.binz@gmail.com>
2018-04-24 14:02:57 +0200

    feat(package): index.ts

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-export {CachingObservableModule} from './src/caching-observable.module';
-export {CachingObservableService} from './src/caching-observable.service';
-export {LocalStorageService} from './src/storage-driver/local-storage.service';
-export {SessionStorageService} from './src/storage-driver/session-storage.service';
+export { CachingObservableModule } from './src/caching-observable.module';
+export { CachingObservableService } from './src/caching-observable.service';
+export { LocalStorageService } from './src/storage-driver/local-storage.service';
+export { SessionStorageService } from './src/storage-driver/session-storage.service';
--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-module.exports = require('./index.d.ts')
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var caching_observable_module_1 = require("./src/caching-observable.module");
+exports.CachingObservableModule = caching_observable_module_1.CachingObservableModule;
+var caching_observable_service_1 = require("./src/caching-observable.service");
+exports.CachingObservableService = caching_observable_service_1.CachingObservableService;
+var local_storage_service_1 = require("./src/storage-driver/local-storage.service");
+exports.LocalStorageService = local_storage_service_1.LocalStorageService;
+var session_storage_service_1 = require("./src/storage-driver/session-storage.service");
+exports.SessionStorageService = session_storage_service_1.SessionStorageService;
+//# sourceMappingURL=index.js.map
